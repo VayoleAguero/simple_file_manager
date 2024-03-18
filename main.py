0b4d5da3e2330a3b7fbed37d1b035ccdb6f65aa7
VayoleAguero <maximk111004@gmail.com>
2024-03-18 21:09:09 +0300

    finished basic version

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def change_folder(folder_name):
 
 def create_file(file_name):
     file_path = os.path.join(working_directory, file_name)
-    with open(file_path, file_name, 'w'):
+    with open(file_path,'w'):
         pass
     print(f"Файл {file_name} успешно создан")
 
@@ -82,3 +82,60 @@ def fileslist():
     files = os.listdir(working_directory)
     for file in files:
         print(file)
+
+mode =0
+while mode!=-1:
+    print('Выберите 1 чтобы создать папку')
+    print('Выберите 2 чтобы удалить папку')
+    print('Выберите 3 чтобы сменить рабочую папку')
+    print('Выберите 4 чтобы создать файл')
+    print('Выберите 5 чтобы записать текст в файл')
+    print('Выберите 6 чтобы посмотреть содержимое файла')
+    print('Выберите 7 чтобы удалить файл')
+    print('Выберите 8 чтобы скопировать файл')
+    print('Выберите 9 чтобы переместить файл')
+    print('Выберите 10 чтобы переименовать файл')
+    print('Выберите 11 чтобы посмотреть список файлов в текущей директории')
+    print('Выберите -1 чтобы выйти из программы')
+    mode= int(input())
+    if mode==1:
+        folder=str(input('Введите имя папки которую хотите создать: '))
+        create_folder(folder)
+    elif mode == 2:
+        folder=str(input('Введите имя папки которую хотите удалить: '))
+        delete_folder(folder)
+    elif mode == 3:
+        print("Введите '..' если хотите перейти на директорию выше или введите название папки в которую хотите переключиться: ")
+        folder=str(input())
+        change_folder(folder)
+    elif mode == 4:
+        file=str(input('Введите имя файла который хотите создать: '))
+        create_file(file)
+    elif mode == 5:
+        file = str(input('Введите имя файла который хотите создать: '))
+        text=str(input('Введите текст который хотите записать в файл: '))
+        write_to_file(file,text)
+    elif mode == 6:
+        file = str(input('Введите имя файла который вы хотите просмотреть: '))
+        view_file(file)
+    elif mode == 7:
+        file = str(input('Введите имя файла который хотите удалить: '))
+        delete_file(file)
+    elif  mode == 8:
+        file = str(input('Введите имя файла который хотите скопировать: '))
+        dest_dir=str(input('Введите имя папки в которую вы хотите скопировать файл'))
+        copy_file(file,dest_dir)
+    elif mode ==9:
+        file = str(input('Введите имя файла который хотите переместить: '))
+        dest_dir = str(input('Введите имя папки в которую вы хотите переместить файл: '))
+        move_file(file,dest_dir)
+    elif mode == 10:
+        file = str(input('Введите имя файла который хотите переименовать: '))
+        new_name=str(input('Введите новое имя файла'))
+        rename_file(file,new_name)
+    elif mode==11:
+        fileslist()
+    elif mode == -1:
+        print("До новых встреч!")
+    else:
+        print("Неизвестная команда")
